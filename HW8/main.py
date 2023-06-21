55c89c12806d699b036ecac31c05690ee663c209
LinaraVasilova <124036734+LinaraVasilova@users.noreply.github.com>
2023-06-21 12:13:02 +0300

    Add files via upload

---
+++ HW8/main.py
@@ -0,0 +1,18 @@
+#w(write) - запись (удаление старой информации)
+
+#a(append) - добавление информации
+
+#r(read) - прочитать информацию
+
+#database = open('data.txt', 'r', encoding='utf-8')
+#print(database.readlines())
+#database.close()
+
+# with open('data.txt', 'r', encoding='utf-8') as database:
+#     print(database.readlines())
+
+# with open('data.txt', 'a', encoding='utf-8') as database:
+#     database.write("\nIvan Ivanov")
+
+with open('data.txt', 'w', encoding='utf-8') as database:
+    database.write("Ivan;Ivanov;89275644125\n")
